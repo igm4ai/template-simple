49e0b696720d6e7535a6e11f04bb8d6e57480012
HansBug <hansbug@buaa.edu.cn>
2022-09-10 13:52:12 +0800

    dev(hansbug): update simple

--- template/igmeta.py
+++ template/igmeta.py
@@ -3,7 +3,7 @@ from igm.conf import igm_project
 igm_project(
     name={{ (user.name | str + '-simple-demo') | potc }},
     version='0.3.2',
-    template_name='igm-simple',
-    template_version='0.0.1',
+    template_name={{ template.name | potc }},
+    template_version={{ template.version | potc }},
     created_at={{ py.time.time() | potc }},
 )
